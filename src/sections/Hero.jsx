acd2f5cc378874d57c7a4a151d6090430908c0e2
Rohan Khengyung <kazkozan38@gmail.com>
2025-12-06 20:21:25 +0545

    initital-c

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
   return (
     <section className='min-h-screen w-full flex flex-col relative'>
       <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
-        <Leva  />
+        {/* <Leva  /> */}
         <p className='sm:text-3xl text-2xl font-medium text-white text-center font-generalsans'>Hi, I am Rohan <span className='waving-hand'>👋</span></p>
         <p className='hero_tag text-gray_gradient'>Building Products & Brands</p>
         <div>
@@ -33,9 +33,9 @@ const Hero = () => {
                     rotation={[0, -Math.PI, 0]} 
                     scale={sizes.deskScale} />
 
-                    {/* <group>
+                    <group>
                       <Target position={sizes.targetPosition} />
-                    </group> */}
+                    </group>
 
                     <ambientLight intensity={1} />
                     <directionalLight position={[10, 10 ,10]} intensity={0.5} />
